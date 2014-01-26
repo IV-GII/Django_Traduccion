a8d689d5bf5c6d0d43a3a7a55028f5a818e51bd7
germaaan <germaaan@gmail.com>
2014-01-26 11:10:39 +0100

    Actualizada receta aprovisionamiento

--- cookbooks/django_trad/recipes/default.rb
+++ cookbooks/django_trad/recipes/default.rb
@@ -1,10 +1,25 @@
 package 'git'
 package 'python'
 
+group "django_trad" do
+	system true
+	action :create	
+end
+
+user "django_trad" do
+	comment "Usuario de la aplicacion"
+	system true
+	shell "/bin/false"
+	action :create
+end
+
 directory "/home/django_trad/" do
-	owner "root"
-	group "root"
-	mode 00777
+	# owner "root"
+	# group "root"
+	# mode 00777
+	owner "django_trad"
+        group "djando_trad"
+	mode 00754
 	action :create
 end
 
@@ -13,3 +28,4 @@ git "/home/django_trad/" do
 	revision "aprov"
 	action :sync
 end
+
