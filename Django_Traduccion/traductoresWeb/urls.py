a486b53ce67c2286ed24c79f7b57a02c822599c1
Iñaki Fernández Janssens <inakitinajo@gmail.com>
2014-01-26 01:19:42 +0100

    Ale, nada de pijadas de urls, ya se cambiará

--- Django_Traduccion/traductoresWeb/urls.py
+++ Django_Traduccion/traductoresWeb/urls.py
@@ -16,8 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
       #url(r'^accounts/profile/', "profile"),
+      url(r'^$', views.index, name='index'),
       url(r'^accounts/', include('allauth.account.urls')),
-      url(r'^files', include('filebaby.urls')),
       url(r'^files/', include('filebaby.urls')),
-      url(r'', views.index, name='index'),
 )
