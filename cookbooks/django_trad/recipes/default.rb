1f948869620fb1def5eadf86d286cbd4c1ccfe31
germaaan <germaaan@gmail.com>
2014-01-26 10:54:51 +0100

    Corregida receta instalación

--- cookbooks/django_trad/recipes/default.rb
+++ cookbooks/django_trad/recipes/default.rb
@@ -1,1 +1,15 @@
+package 'git'
 package 'python'
+
+directory "/home/django_trad/" do
+	owner "root"
+	group "root"
+	mode 00777
+	action :create
+end
+
+git "/home/django_trad/" do
+	repository "https://github.com/IV-GII/Django_Traduccion.git"
+	revision "aprov"
+	action :sync
+end
