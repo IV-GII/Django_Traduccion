59d0ed45bef37b9fe8a3a4b980ad47beaac34c66
germaaan <germaaan@gmail.com>
2014-01-29 15:25:11 +0100

    Actualizados fuentes y eliminados archivos nulos

--- filebaby/urls.py
+++ filebaby/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 
-from filebaby.views import FileListView, FileAddView, FileAddHashedView
+from filebaby.views import FileListView, FileAddView, FileAddHashedView, FileDownloadView, FileSendView, DeleteFileView, ExtractFileView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,14 +15,20 @@ urlpatterns = patterns('',
     # url(r'^uploadering/', include('uploadering.foo.urls')),
     #
     # This is the view that handles the file uploads
-    url(r'^add/?$', FileAddView.as_view(), name='filebaby-add'),
+    #url(r'^add/?$', FileAddView.as_view(), name='filebaby-add'),
     #
     # This view hashes uploads to create directories
-    #url(r'^add$', FileAddHashedView.as_view(), name='filebaby-add'),
+    url(r'^add$', FileAddHashedView.as_view(), name='filebaby-add'),
     #
     # This view lists uploaded files
     url(r'^$', FileListView.as_view(), name='home'),
-
+#    url(r'^download/(?P<query>\w+)$', FileDownloadView.as_view(), name='home'),
+#    url(r'^download/(\?file)(\w{32})$', FileDownloadView.as_view(), name='home'),
+    url(r'^download/(.*)', FileDownloadView.as_view(), name='home'),
+    url(r'^send/(.*)', FileSendView.as_view(), name='home'),
+    url(r'^delete/(.*)', DeleteFileView.as_view(), name='home'),
+    url(r'^extract/(.*)', ExtractFileView.as_view(), name='home'),
+#    url(r'^download/(\w)$', FileDownloadView.as_view(), name='home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -30,5 +36,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-# Cambiar y poner MEDIA_URL Y MEDIA_ROOT EN settings
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
