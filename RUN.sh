3b6af3d1799b94bd73ae868a781b6c429dd4be52
germaaan <germaaan@gmail.com>
2014-01-31 18:34:41 +0100

    Eliminados archivos sobrantes

--- RUN.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Script para ejecución de la aplicación
-
-chef-solo -c solo.rb -j node.json
--- solo.rb
+++
@@ -1,2 +0,0 @@
-cookbook_path File.expand_path("../cookbooks", __FILE__)
-json_attribs File.expand_path("../node.json", __FILE__)
